f835b792d777e2edb90a65cdca4ebc124b38ca75
Phillarmonkey <phillarmonkey@outlook.com>
2025-03-25 20:17:15 +0000

    WIP

--- src/DependencyInjection/AllegroRedisOdmExtension.php
+++ src/DependencyInjection/AllegroRedisOdmExtension.php
@@ -3,14 +3,14 @@
 namespace Phillarmonic\AllegroRedisOdmBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Definition;
 
 class AllegroRedisOdmExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
